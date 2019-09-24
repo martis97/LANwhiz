558078eb302c36ffa86ffcb6b9146e652cb5a957
Martynas Markevičius <martynas.markevicius97@gmail.com>
2019-09-25 00:53:30 +0100

    Left behind from the last commit

--- utils.py
+++ utils.py
@@ -90,54 +90,6 @@ class Utilities(object):
 
         return ".".join(octets)
 
-# 'bgp_time_conversion',
-# 'cli',
-# 'close',
-# 'commit_config',
-# 'compare_config',
-# 'compliance_report',
-# 'connection_tests',
-# 'dest_file_system',
-# 'device',
-# 'discard_config',
-# 'get_arp_table',
-# 'get_bgp_config',
-# 'get_bgp_neighbors',
-# 'get_bgp_neighbors_detail',
-# 'get_config',
-# 'get_environment',
-# 'get_facts',
-# 'get_firewall_policies',
-# 'get_interfaces',
-# 'get_interfaces_counters',
-# 'get_interfaces_ip',
-# 'get_ipv6_neighbors_table',
-# 'get_lldp_neighbors',
-# 'get_lldp_neighbors_detail',
-# 'get_mac_address_table',
-# 'get_network_instances',
-# 'get_ntp_peers',
-# 'get_ntp_servers',
-# 'get_ntp_stats',
-# 'get_optics',
-# 'get_probes_config',
-# 'get_probes_results',
-# 'get_route_to',
-# 'get_snmp_information',
-# 'get_users',
-# 'is_alive',
-# 'load_merge_candidate',
-# 'load_replace_candidate',
-# 'load_template',
-# 'open',
-# 'parse_uptime',
-# 'ping',
-# 'post_connection_tests',
-# 'pre_connection_tests',
-# 'rollback',
-# 'traceroute'
-
-
     def cidr_to_wildcard_mask(self, cidr):
         """ Convert CIDR to Wildcard Mask
         
@@ -179,7 +131,6 @@ class Utilities(object):
             self.send_command("conf t")
         elif re.match(r"^[A-Za-z0-9\-]+\#$", prompt):
             self.send_command("conf t")
-
 
     @staticmethod 
     def get_all_devices():
